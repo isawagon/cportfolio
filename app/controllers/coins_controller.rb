2e45265ac1eedef12f69afad31f2c551e3f46b9e
isabelle levy <isabel.levy@icloud.com>
2022-09-13 15:48:34 +0200

    add feature new portfolio

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,5 @@
 class CoinsController < ApplicationController
-  require "open-uri"
+  
 
   def index
   end
@@ -10,11 +10,5 @@ class CoinsController < ApplicationController
   def show
   end
 
-  private
 
-  def search_price(coin)
-    price_url = "https://api.coingecko.com/api/v3/simple/price?ids=#{coin.gecko_coin}&vs_currencies=EUR&include_24hr_change=true"
-    price_serialized = URI.open(price_url).read
-    JSON.parse(price_serialized)
-  end
 end
