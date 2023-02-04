c554153b26fb10a02ecf1a22b8d04f0f9f71b504
isabelle levy <isabel.levy@icloud.com>
2023-02-04 15:13:59 +0100

    mutualiser appel api carac

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,21 +2,38 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
   require "open-uri"
 
+  def about
+  end
+
   def home
+  # préparer la liste des gecko_coin
+  # appeler l'API
+  # associer le price à chaque coin
+  # a faire :
+    # envoyer à la view un hash avec uniquement les données nécessaires
+      # ptf coin
+      # ptf_id coin_id logo symbol name ptf_id stock price + la value de la ligne
+      # cela permettra de supprimer les données prix de la DB,
+      # et d'optimiser le code de home.html.erb
+    # trier les coins par valeur décroissante dans chaque ptf
+
     @portfolios = current_user.portfolios.all
     @coins = current_user.coins.all
+    list = "#{@coins.first.gecko_coin}"
     @coins.each do |coin|
-      prices = search_price(coin)
+      list = "#{list},#{coin.gecko_coin}"
+    end
+    prices = search_price(list)
+    @coins.each do |coin|
       coin.price = prices[coin.gecko_coin]['eur']
       coin.change = prices[coin.gecko_coin]['eur_24h_change']
-      # coin.save!
     end
   end
 
   private
 
-  def search_price(coin)
-    price_url = "https://api.coingecko.com/api/v3/simple/price?ids=#{coin.gecko_coin}&vs_currencies=EUR&include_24hr_change=true"
+  def search_price(list)
+    price_url = "https://api.coingecko.com/api/v3/simple/price?ids=#{list}&vs_currencies=EUR&include_24hr_change=true"
     price_serialized = URI.open(price_url).read
     JSON.parse(price_serialized)
   end
