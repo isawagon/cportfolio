3308cf396138176465dd186978613a5e5550f1da
isabelle levy <isabel.levy@icloud.com>
2023-02-09 01:15:20 +0100

    ajout to do

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,30 +6,24 @@ class PagesController < ApplicationController
   end
 
   def home
-  # préparer la liste des gecko_coin
+
   # appeler l'API
   # associer le price à chaque coin
-  # a faire :
-    # envoyer à la view un hash avec uniquement les données nécessaires
-      # ptf coin
-      # ptf_id coin_id logo symbol name ptf_id stock price + la value de la ligne
-      # cela permettra de supprimer les données prix de la DB,
-      # et d'optimiser le code de home.html.erb
-    # trier les coins par valeur décroissante dans chaque ptf
+  # a faire optional:
+    # supprimer les données prix de la DB,
+    # mettre les données ptf dans @dashboard et ne plus envoyer @portfolios à la vue
+    # appel à une method pour constituer les hash des coins
 
+    # préparer la liste des gecko_coin et appel API pour récupérer les cours
     @portfolios = current_user.portfolios.all
-    @coins = current_user.coins.all
-    list = "#{@coins.first.gecko_coin}"
-    @coins.each do |coin|
+    coins = current_user.coins.all
+    list = "#{coins.first.gecko_coin}"
+    coins.each do |coin|
       list = "#{list},#{coin.gecko_coin}"
     end
     prices = search_price(list)
-    # @coins.each do |coin|
-    #   coin.price = prices[coin.gecko_coin]['eur']
-    #   coin.change = prices[coin.gecko_coin]['eur_24h_change']
-    # end
 
-    # créer la nouvelle liste des coins
+    # constituer les données dashboard
     @dashboard = {}
     current_user.portfolios.all.each do |portfolio|
       c3 = {}
@@ -42,21 +36,22 @@ class PagesController < ApplicationController
       c2.each do |coin|
         c3["#{coin.id}"] =
           { id: coin.id,
-          portfolio_id: coin.portfolio_id,
-          image_url: coin.image_url,
-          name: coin.name,
-          stock:  coin.stock,
-          price: prices[coin.gecko_coin]['eur'],
-          value: coin.stock * prices[coin.gecko_coin]['eur'] }
+            portfolio_id: coin.portfolio_id,
+            image_url: coin.image_url,
+            name: coin.name,
+            stock: coin.stock,
+            price: prices[coin.gecko_coin]['eur'],
+            value: coin.stock * prices[coin.gecko_coin]['eur'] }
         total += coin.stock * prices[coin.gecko_coin]['eur']
       end
 
       # trier c3 par value DESC
+      c3 = c3.sort_by { |_k, v| v[:value].to_f }.reverse.to_h
+
       @dashboard["#{portfolio.id}"]["assets"] = c3
       @dashboard["#{portfolio.id}"]["total_ptf"] = total
     end
-
-# fin créer
+    # fin constituer les données dashboard
   end
 
   private
